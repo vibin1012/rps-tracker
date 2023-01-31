685d130cc505b3916ac0703e1563098cf3795a75
vibin1012 <122555614+vibin1012@users.noreply.github.com>
2023-01-31 11:45:45 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("A")
+    PA += 1
+    Rounds += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("T")
+    Ties += 1
+    Rounds += 1
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("B")
+    PB += 1
+    Rounds += 1
+})
+input.onGesture(Gesture.Shake, function () {
+    reset()
+})
 function updatescore () {
     OLED.clear()
     OLED.writeStringNewLine("PLayer A:" + PA)
@@ -8,15 +26,18 @@ function updatescore () {
     OLED.newLine()
     OLED.writeStringNewLine("Ties:" + Ties)
 }
+function reset () {
+    OLED.init(128, 64)
+    PA = 0
+    PB = 0
+    Rounds = 0
+    Ties = 0
+    OLED.writeStringNewLine("play")
+    basic.pause(2000)
+    updatescore()
+}
+let PB = 0
 let Ties = 0
 let Rounds = 0
-let PB = 0
 let PA = 0
-OLED.init(128, 64)
-PA = 0
-PB = 0
-Rounds = 0
-Ties = 0
-OLED.writeStringNewLine("play")
-basic.pause(2000)
-updatescore()
+reset()
